ba31058a18224726d2d8fdb3bd2542889516bf7f
Carlos Ferreira <conta@especializati.com.br>
2022-01-18 11:32:47 -0300

    Create Home Page

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="home">
-    Listagem dos cursos
+  <div>
+    <div class="pageTitle">
+            <span class="title">Meus Cursos</span>
+            <span class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+
+        <div class="content">
+            <ul class="listCourses">
+                <card-course
+                  :title="'Curso de Laravel com Vue.js'"
+                  :image="''"
+                  :bgName="'laravel'">
+                </card-course>
+            </ul>
+        </div>
   </div>
 </template>
 
 <script>
+import CardCourse from './components/CardCouse.vue'
+
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  components: {
+    CardCourse
+  }
+};
 </script>
---
+++ src/views/home/components/CardCouse.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <span :class="['card', 'bg-' + bgName]">
+      <span class="icon">
+        <img :src="[image != '' ? image : require('@/assets/images/icons/default.png')]" :alt="title" />
+      </span>
+      <span class="title">{{ title }}</span>
+      <span class="action">
+        <a href="trainings.html" :class="['btn', bgName]">Acessar</a>
+      </span>
+    </span>
+    <span class="dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "CardCourse",
+  props: ['title', 'image', 'bgName']
+};
+</script>
