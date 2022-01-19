83d4efd3b077e4fd04472acba67e4bc09df7acf3
Carlos Ferreira <conta@especializati.com.br>
2022-01-19 10:32:15 -0300

    Create Page My Supports

--- src/views/supports/MySupports.vue
+++ src/views/supports/MySupports.vue
@@ -1,11 +1,51 @@
 <template>
-  <div class="home">
-    Minhas dúvidas
+  <div>
+    <div class="pageTitle">
+      <span class="title">Minhas Dúvidas</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
+
+    <div class="content">
+      <div class="container">
+        <div class="left">
+          <div class="card">
+            <div class="title bg-laravel">
+              <span class="text">Filtros</span>
+            </div>
+            <div class="modules">
+              <ul class="classes">
+                <li>Todos</li>
+                <li>Aguardando Minha Resposta</li>
+                <li>Aguardando Professor</li>
+                <li>Finalizados</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="right">
+          <div class="content">
+            <div class="comments">
+              <supports />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Supports from "@/components/Supports.vue";
+
 export default {
-  name: 'MySupports',
-}
+  name: "MySupports",
+  components: {
+    Supports,
+  },
+};
 </script>
