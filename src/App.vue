1e3827e7edb54803c19fff377b12937c56d91cc5
Carlos Ferreira <conta@especializati.com.br>
2022-02-01 15:59:24 -0300

    Fix icons

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
-@import url('https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p');
+@import url('https://pro.fontawesome.com/releases/v5.10.0/css/all.css');
 </style>
 
 <style lang="scss">
