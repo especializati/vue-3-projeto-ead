3cb8ae7a1d657b23ab8939ab79a2206f6364a5cc
Carlos Ferreira <conta@especializati.com.br>
2022-02-01 16:30:27 -0300

    List Modules and Lessons

--- src/views/modules/ModulesAndLessons.vue
+++ src/views/modules/ModulesAndLessons.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="pageTitle">
-            <span class="title">Curso X</span>
+            <span class="title">{{ course.name }}</span>
             <span class="dots">
                 <span></span>
                 <span></span>
@@ -28,12 +28,24 @@
 </template>
 
 <script>
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
 import Modules from './components/Modules.vue'
 import Player from './components/Player.vue'
 import SupportsLesson from './components/Supports.vue'
 
 export default {
     name: 'ModulesAndLessons',
+    setup() {
+        const store = useStore()
+
+        const course = computed(() => store.state.courses.courseSelected)
+
+        return {
+            course
+        }
+    },
     components: {
         Modules,
         Player,
--- src/views/modules/components/Modules.vue
+++ src/views/modules/components/Modules.vue
@@ -5,55 +5,40 @@
         <span class="text">Modulos</span>
         <span class="icon far fa-stream"></span>
       </div>
-      <div class="modules">
+
+      <div class="modules" v-for="module in modules" :key="module.id">
         <div class="name">
-          <span class="text">Modulo 1</span>
+          <span class="text">{{ module.name }}</span>
           <span class="icon fas fa-sort-down"></span>
         </div>
         <ul class="classes">
-          <li class="active">
+          <li class="active" v-for="lesson in module.lessons" :key="lesson.id">
             <span class="check active fas fa-check"></span>
-            <span class="nameLesson">Aula 01</span>
-          </li>
-          <li>
-            <span class="check active fas fa-check"></span>
-            <span class="nameLesson">Aula 02</span>
-            <span class="file fas fa-file-archive"></span>
-          </li>
-          <li>
-            <span class="check fas fa-check"></span>
-            <span class="nameLesson">Aula 03</span>
-            <span class="file fas fa-file-archive"></span>
+            <span class="nameLesson">{{ lesson.name }}</span>
           </li>
         </ul>
       </div>
-      <div class="modules">
-        <div class="name">
-          <span class="text">Modulo 2</span>
-          <span class="icon fas fa-sort-down"></span>
-        </div>
-        <ul class="classes">
-          <li>
-            <span class="check fas fa-check"></span>
-            <span class="nameLesson">Aula 01</span>
-          </li>
-          <li>
-            <span class="check active fas fa-check"></span>
-            <span class="nameLesson">Aula 02</span>
-            <span class="file fas fa-file-archive"></span>
-          </li>
-          <li>
-            <span class="check fas fa-check"></span>
-            <span class="nameLesson">Aula 03</span>
-          </li>
-        </ul>
-      </div>
+      
     </div>
   </div>
 </template>
 
 <script>
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
 export default {
-    name: 'Modules'
+    name: 'Modules',
+    setup() {
+        const store = useStore()
+
+        // const course = computed(() => store.state.courses.courseSelected)
+        const modules = computed(() => store.state.courses.courseSelected.modules)
+
+        return {
+          // course,
+          modules,
+        }
+    },
 }
 </script>
