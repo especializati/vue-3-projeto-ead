26b1946a28b684e810eb925aee8d735912ae2335
Carlos Ferreira <conta@especializati.com.br>
2022-02-02 15:40:51 -0300

    List Supports

--- src/components/Supports.vue
+++ src/components/Supports.vue
@@ -1,57 +1,40 @@
 <template>
   <div class="content">
-    <div class="card">
+
+    <div class="card" v-for="support in supports.data" :key="support.id">
       <div class="commentContent main">
         <span class="avatar">
-          <img src="images/avatars/user01.svg" alt="" />
-        </span>
-        <span class="comment">
-          <div class="balloon">
-            <span class="fas fa-sort-down"></span>
-            <span class="owner">Fernando - 07/10/2021</span>
-            <span class="text">
-              Donec ligula libero, sollicitudin vel libero vel, mollis porttitor
-              turpis. Cras mattis turpis massa, sit amet fringilla diam auctor
-              ac. Integer sit amet rutrum risus. In eleifend urna sapien,
-              faucibus pharetra justo luctus quis. Vivamus eleifend fringilla
-              massa
-            </span>
-          </div>
-        </span>
-        <button class="btn primary">Ver respostas</button>
-      </div>
-    </div>
-    <div class="card">
-      <div class="commentContent main">
-        <span class="avatar">
-          <img src="images/avatars/user01.svg" alt="" />
+          <img :src="[
+            support.user.image ?
+            support.user.image :
+            '@/assets/images/avatars/user01.svg'
+          ]" :alt="support.user.name" />
         </span>
         <div class="comment">
           <div class="balloon">
             <span class="fas fa-sort-down"></span>
-            <span class="owner">Fernando - 07/10/2021</span>
-            <span class="text">
-              In eleifend urna sapien, faucibus pharetra justo luctus quis.
-              Vivamus eleifend fringilla massa
-            </span>
+            <span class="owner">{{ support.user.name }} - {{ support.dt_updated }}</span>
+            <span class="text">{{ support.description }}</span>
           </div>
         </div>
         <button class="btn primary">Ocultar respostas</button>
       </div>
-      <div class="answersContent">
+      <div class="answersContent" v-for="reply in support.replies" :key="reply.id">
         <div class="commentContent rightContent">
           <div class="comment">
             <div class="balloon">
               <span class="fas fa-sort-down"></span>
-              <span class="owner">Carlos Ferreira - 07/10/2021</span>
-              <span class="text">
-                In eleifend urna sapien, faucibus pharetra justo luctus quis.
-                Vivamus eleifend fringilla massa
-              </span>
+              <span class="owner">{{ reply.user.name }} - {{ reply.date }}</span>
+              <span class="text" v-text="reply.description"></span>
             </div>
           </div>
           <span class="avatar">
-            <img src="images/avatars/user03.svg" alt="" />
+            <img :src="
+              [
+              reply.user.image ?
+              reply.user.image :
+              '@/assets/images/avatars/user01.svg'
+            ]" :alt="reply.user.name" />
           </span>
         </div>
         <div class="commentContent">
@@ -74,30 +57,24 @@
         </span>
       </div>
     </div>
-    <div class="card">
-      <div class="commentContent main">
-        <span class="avatar">
-          <img src="images/avatars/user02.svg" alt="" />
-        </span>
-        <span class="comment">
-          <div class="balloon">
-            <span class="fas fa-sort-down"></span>
-            <span class="owner">João - 06/10/2021</span>
-            <span class="text">
-              Integer scelerisque placerat molestie. Vivamus dignissim bibendum
-              sapien, non suscipit arcu lobortis nec. Donec eros est, mollis
-              quis nibh quis, pellentesque suscipit libero.
-            </span>
-          </div>
-        </span>
-        <button class="btn primary">Ver respostas</button>
-      </div>
-    </div>
+    
   </div>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useStore } from 'vuex'
+
 export default {
   name: "Supports",
+  setup() {
+    const store = useStore()
+
+    const supports = computed(() => store.state.supports.supports)
+
+    return {
+      supports
+    }
+  }
 };
 </script>
