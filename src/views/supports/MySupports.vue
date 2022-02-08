f7cd6874367ac6d26b97fed80dbb373405bd298e
Carlos Ferreira <conta@especializati.com.br>
2022-02-08 10:28:02 -0300

    Filter Supports By Status

--- src/views/supports/MySupports.vue
+++ src/views/supports/MySupports.vue
@@ -18,10 +18,10 @@
             </div>
             <div class="modules">
               <ul class="classes">
-                <li>Todos</li>
-                <li>Aguardando Minha Resposta</li>
-                <li>Aguardando Professor</li>
-                <li>Finalizados</li>
+                <li :class="{active : status === ''}" @click="getMySupportsWithStatus('')">Todos</li>
+                <li :class="{active : status === 'A'}" @click="getMySupportsWithStatus('A')">Aguardando Minha Resposta</li>
+                <li :class="{active : status === 'P'}" @click="getMySupportsWithStatus('P')">Aguardando Professor</li>
+                <li :class="{active : status === 'C'}" @click="getMySupportsWithStatus('C')">Finalizados</li>
               </ul>
             </div>
           </div>
@@ -56,6 +56,17 @@ export default {
     const status = ref('')
 
     onMounted(() => store.dispatch('getMySupports', status.value))
+
+    const getMySupportsWithStatus = (newStatus) => {
+      status.value = newStatus
+
+      store.dispatch('getMySupports', newStatus)
+    }
+
+    return {
+      getMySupportsWithStatus,
+      status
+    }
   }
 };
 </script>
