ab93f863eb8847b72d7bf79cbe76c5a0171815de
Carlos Ferreira <conta@especializati.com.br>
2022-02-09 11:48:41 -0300

    Create Menu

--- src/layouts/components/HeaderComponent.vue
+++ src/layouts/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <header id="header">
         <nav>
-            <span class="toggleMenu far fa-bars"></span>
+            <span class="toggleMenu far fa-bars" @click="toggleMenu"></span>
             <span class="logo">
                 <router-link :to="{name: 'campus.home'}">
                     <img :src="require('@/assets/images/logo.svg')" alt="EspecializaTi">
@@ -9,10 +9,30 @@
             </span>
         </nav>
     </header>
+
+    <menu-component v-show="showMenu">
+    </menu-component>
 </template>
 
 <script>
+import { ref } from 'vue'
+
+import MenuComponent from './MenuComponent.vue'
+
 export default {
-    name: 'header-component'
+    name: 'header-component',
+    setup() {
+        const showMenu = ref(false)
+
+        const toggleMenu = () => showMenu.value = !showMenu.value
+
+        return {
+            showMenu,
+            toggleMenu,
+        }
+    },
+    components: {
+        MenuComponent
+    }
 }
 </script>
---
+++ src/layouts/components/MenuComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <transition name="fade">
+            <ul id="mainMenu">
+                <li>
+                    <router-link :to="{name: 'campus.home'}">Início</router-link>
+                </li>
+                <li>
+                    <router-link :to="{name: 'campus.my.supports'}">Minhas Dúvidas</router-link>
+                </li>
+                <li>
+                    <a href="">Sair</a>
+                </li>
+            </ul>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Menu',
+}
+</script>
